3d5f9803a6d24eb8a9317c8da08627dd7c37aca0
vishal <vishalnobita005@gmail.com>
2024-05-02 22:20:00 +0530

    Your descriptive commit message here

--- src/_test_/login.text.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Login from './Login';
-
-test('renders login component', () => {
-  render(<Login />);
-  const loginHeader = screen.getByText(/Login/i);
-  expect(loginHeader).toBeInTheDocument();
-});
